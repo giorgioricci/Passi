14b48873dd52fcc9c1785b73e27f3e4c5fc8ef08
Giorgio <gio.ricci@gmail.com>
2022-01-06 14:27:53 +0100

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 input.onGesture(Gesture.Shake, function () {
     Passi += 1
+    music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
     led.stopAnimation()
 })
 input.onButtonPressed(Button.AB, function () {
     Passi = 0
+    music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
 })
 let Passi = 0
 Passi = 0
